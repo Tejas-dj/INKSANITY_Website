9ebb6aa19094080562cf68c1c209c1f82caa3e42
Tejas-dj <tejas.dj@outlook.com>
2026-02-17 11:05:23 +0530

    in progress message

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <h1 className="text-4xl font-bold">Inksanity Setup Complete</h1>
+      <h1 className="text-4xl font-bold">Inksanity Setup In Progress</h1>
     </main>
   );
 }
